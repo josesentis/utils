525d1fc477a26ff391c6d7e8adb9e537abd0d3b5
Jose Sentis <jose.sentis1987@gmail.com>
2020-04-19 19:32:05 +0200

    Iterates javascript

--- scrollSkew.js
+++ scrollSkew.js
@@ -1,3 +1,4 @@
+
 let currentScroll = document.pageYOffset;
 
 const scrollSkewer = () => {
@@ -6,12 +7,10 @@ const scrollSkewer = () => {
     const diff = newScroll - currentScroll;
     const speed = diff * 0.15;
 
-    currentScroll = newScroll
-
+    currentScroll = newScroll;
     document.body.transform = `skewY(${speed}deg)`;
   }
-
-  requestAnimationFrame(scrollSkewer);
 };
 
-export default scrollSkewer;
+scrollSkewer();
+const animationFrame = requestAnimationFrame(scrollSkewer);
