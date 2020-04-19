c16450a2224202a482a8dcb2c97394ffb0e32428
Jose <jose@runroom.com>
2020-04-19 18:52:26 +0200

    Adds scrollSkew function

---
+++ scrollSkew.js
@@ -0,0 +1,17 @@
+let currentScroll = document.pageYOffset;
+
+const scrollSkewer = () => {
+    if (currentScroll !== newScroll) {
+        const newScroll = window.pageYOffset;
+        const diff = newScroll - currentScroll;
+        const speed = diff * 0.15;
+
+        currentScroll = newScroll
+
+        document.body.transform = `skewY(${speed}deg)`;
+    }
+
+    requestAnimationFrame(scrollSkewer);
+};
+
+scrollSkewer();
