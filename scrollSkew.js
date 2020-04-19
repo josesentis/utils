e12cb4d6c1342d709411fac460cdd4b967dbb799
Jose Sentis <jose.sentis1987@gmail.com>
2020-04-19 19:23:07 +0200

    Prettifies js

--- scrollSkew.js
+++ scrollSkew.js
@@ -1,17 +1,17 @@
 let currentScroll = document.pageYOffset;
 
 const scrollSkewer = () => {
-    if (currentScroll !== newScroll) {
-        const newScroll = window.pageYOffset;
-        const diff = newScroll - currentScroll;
-        const speed = diff * 0.15;
+  if (currentScroll !== newScroll) {
+    const newScroll = window.pageYOffset;
+    const diff = newScroll - currentScroll;
+    const speed = diff * 0.15;
 
-        currentScroll = newScroll
+    currentScroll = newScroll
 
-        document.body.transform = `skewY(${speed}deg)`;
-    }
+    document.body.transform = `skewY(${speed}deg)`;
+  }
 
-    requestAnimationFrame(scrollSkewer);
+  requestAnimationFrame(scrollSkewer);
 };
 
-scrollSkewer();
+export default scrollSkewer;
