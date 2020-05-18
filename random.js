18c4e19e7f43967b166fdf1bb2c95dc5757a6757
Jose Sentis <jose.sentis1987@gmail.com>
2020-05-18 19:48:56 +0200

    Create random.js

---
+++ random.js
@@ -0,0 +1,7 @@
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomFromInterval(min, max) {
+  return Math.random() * (max - min) + min;
+}
